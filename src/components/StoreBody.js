b8f1f3d0c35df90122c7489feb80d5a35d450db3
tochibedford <musicofbyte@gmail.com>
2022-03-27 20:58:13 +0100

    optimized shop & model viewer orbit constraints

--- src/components/StoreBody.js
+++ src/components/StoreBody.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Item from './Item';
-import capModel from '../models/cap.glb';
+import capModel1 from '../models/brown1.glb';
+import capModel2 from '../models/blue1.glb';
+import capModel3 from '../models/whiteBlack1.glb';
 import capImage from '../models/cap.png'
 
 const StoreBody = () => {
     return (
         <div className="storeBody">
-            <Item  model={capModel} modelPoster={capImage}/>
-            <Item  model={capModel} modelPoster={capImage}/>
-            <Item  model={capModel} modelPoster={capImage}/>
+            <Item  model={capModel1} modelPoster={capImage}/>
+            <Item  model={capModel2} modelPoster={capImage}/>
+            <Item  model={capModel3} modelPoster={capImage}/>
         </div>
     )
 }
