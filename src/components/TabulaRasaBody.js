2288f36ecdaadad7c409cbe82e2a69676e8cd8b7
tochibedford <musicofbyte@gmail.com>
2022-03-30 06:32:30 +0100

    switched to react-canvas-draw for tabula-rasa and also started using react-device-detect to detect mobile

--- src/components/TabulaRasaBody.js
+++ src/components/TabulaRasaBody.js
@@ -1,127 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react'
+import CanvasDraw from "react-canvas-draw";
+import {isMobile} from 'react-device-detect';
 
 const TabulaRasaBody = () => {
-    
-    const canvasRef = useRef(null);
-    const contextRef = useRef(null);
-    const [isDrawing, setIsDrawing] = useState(false);
-    const [color, setColor] = useState("rgb(0,0,0)");
-    const [lineWidth, setLineWidth] = useState(3);
-    const [lineOpacity, setLineOpacity] = useState(0.1);
-
-    //get current position of mouse
-    function getMousePosition(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        }
-    }
-
-    var touchAvailable = ('createTouch' in document) || ('onstarttouch' in window);
 
     // 0 to 255;
     const number255Generator = () => {
         return Math.floor(Math.random() * 256);
     }
 
-    useEffect(()=>{
-        const canvas = canvasRef.current;
-        var scale = window.devicePixelRatio;
-        const canvasPadding = 50;
-
-        // code to support higher pixel density
-        canvas.width = Math.floor((canvas.parentNode.offsetWidth - canvasPadding) * scale);
-        canvas.height = Math.floor((canvas.parentNode.offsetHeight - canvasPadding) * scale);
-        canvas.style.width = `${canvas.parentNode.offsetWidth - canvasPadding}px`;
-        canvas.style.height = `${canvas.parentNode.offsetHeight - canvasPadding}px`;
-
-        const context = canvas.getContext('2d');
-        context.scale = (scale, scale);
-        context.lineCap = 'round';
-        context.lineJoin = 'round';
-        context.strokeStyle = color;
-        context.lineWidth = lineWidth;
-        context.globalAlpha = lineOpacity;
-        contextRef.current = context;
-
-        canvas.addEventListener('touchstart', function (e) {
-            if (e.target === canvas) {
-                e.preventDefault();
-            }
-        }, { passive: false });
-
-        canvas.addEventListener('touchend', function (e) {
-            if (e.target === canvas) {
-                e.preventDefault();
-            }
-        }, { passive: false });
-
-        canvas.addEventListener('touchmove', function (e) {
-            if (e.target === canvas) {
-                e.preventDefault();
-            }
-        }, { passive: false });
-
-        canvas.addEventListener('touchstart',function(e){
-            var mousePos = getMousePosition(canvas,e)
-            var touch = e.touches[0];
-            var mouseEvent =  new MouseEvent("mousedown",{
-                clientX:touch.clientX,
-                clientY:touch.clientY
-            });
-            console.log(mouseEvent)
-            canvas.dispatchEvent(mouseEvent)
-        },false);
-        canvas.addEventListener('touchend', function (e) {
-            var mouseEvent = new MouseEvent("mouseup", {});
-            canvas.dispatchEvent(mouseEvent)
-        }, false);
-        canvas.addEventListener('touchmove', function (e) {
-            var touch = e.touches[0];
-            var mouseEvent = new MouseEvent("mousemove", {
-                clientX: touch.clientX,
-                clientY: touch.clientY
-            });
-            canvas.dispatchEvent(mouseEvent)
-        }, false);
-    }, []);
-
-    const startDrawing = ({nativeEvent})=>{
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(offsetX, offsetY);
-        setIsDrawing(true);
-        return false;
-    }
-
-    const finishDrawing = ()=>{
-        contextRef.current.closePath();
-        setColor(`rgb(${number255Generator()}, ${number255Generator()}, ${number255Generator()})`)
-        contextRef.current.strokeStyle = color;
-        setIsDrawing(false);
-        return false;
-    }
-
-    const draw = ({nativeEvent})=>{
-        if(!isDrawing){
-            return
-        }
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
-        return false;
-    }
-
     return (
         <div className="tabulaRasaBody">
-            <canvas
-                ref={canvasRef}
-                onMouseDown={startDrawing}
-                onMouseUp={finishDrawing}
-                onMouseMove={draw}
-                className="tabulaCanvas"
-            />
+            <CanvasDraw />
         </div>
     )
 }
